cc626f628c5e09b46ecf7053d933392eb898132d
lana <sitdownrightnow.dev@gmail.com>
2021-03-29 10:49:58 +0700

    feat: add database context and initializer

---
+++ Database/DatabaseInitializer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinformsLibraryManager.Model;
+
+namespace WinformsLibraryManager.Database
+{
+    public class DatabaseInitializer : CreateDatabaseIfNotExists<ModelContext>
+    {
+    }
+}
---
+++ Model/ModelContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinformsLibraryManager.Database;
+
+namespace WinformsLibraryManager.Model
+{
+    public class ModelContext : DbContext
+    {
+        public ModelContext() : base("SchoolDBConnectionString")
+        {
+            System.Data.Entity.Database.SetInitializer(new DatabaseInitializer());
+        }
+    }
+}
