632293d96e5297e383ebe7c9c4b9758ce8066d63
lana <sitdownrightnow.dev@gmail.com>
2021-03-30 08:58:57 +0700

    feat: base users model

--- Model/User.cs
+++ Model/User.cs
@@ -1,7 +1,22 @@
-﻿namespace WinformsLibraryManager.Model
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WinformsLibraryManager.Model
 {
-    public class User
+    public class User : Model
     {
+        [Required] public string Name { get; set; }
+
+        [Required] public string Username { get; set; }
+
+        [Required] public string Password { get; set; }
         
+        public virtual ICollection<Permission> Permissions { get; set; }
+    }
+
+    public class Permission : Model, INamed
+    {
+        [Index(IsUnique = true), Required] public string Name { get; set; }
     }
 }
