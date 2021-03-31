c29afb55b1c6d0a91349b69ef52edb07820a4de4
lana <sitdownrightnow.dev@gmail.com>
2021-04-01 00:16:15 +0700

    feat: add model to context

--- Model/ModelContext.cs
+++ Model/ModelContext.cs
@@ -1,16 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using WinFormsLibraryManager.Database;
+using WinFormsLibraryManager.Model.Book;
+using WinFormsLibraryManager.Model.Meta;
 
 namespace WinFormsLibraryManager.Model
 {
     public class ModelContext : DbContext
     {
+        public DbSet<BookMeta> Books { get; set; }
+        public DbSet<BookItem> BookItems { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<Reader> Readers { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<Series> Series { get; set; }
+        public DbSet<Storage> Storages { get; set; }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Permission> Permissions { get; set; }
+
         public ModelContext()
         {
             System.Data.Entity.Database.SetInitializer(new DatabaseInitializer());
