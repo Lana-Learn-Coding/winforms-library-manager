f63fea83d64114daa4225b92da7168b456519f97
lana <sitdownrightnow.dev@gmail.com>
2021-04-09 00:02:31 +0700

    feat: add named entity combobox

---
+++ UI/Component/NamedEntityCombobox.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace WinFormsLibraryManager.UI.Component
+{
+    public partial class NamedEntityCombobox : CreateableComboBox
+    {
+        public NamedEntityCombobox() : this(null)
+        {
+        }
+
+        public NamedEntityCombobox(IContainer container) : base(container)
+        {
+        }
+    }
+}
