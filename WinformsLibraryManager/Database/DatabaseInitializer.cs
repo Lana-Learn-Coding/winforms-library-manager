02ef4caed50ab4d26135b99fd6c58437ca4a51a4
lana <sitdownrightnow.dev@gmail.com>
2021-04-09 12:30:45 +0700

    migrate: move project to sub folder

---
+++ WinformsLibraryManager/Database/DatabaseInitializer.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using WinFormsLibraryManager.Model;
+using WinFormsLibraryManager.Model.Meta;
+
+namespace WinFormsLibraryManager.Database
+{
+    public class DatabaseInitializer : CreateDatabaseIfNotExists<ModelContext>
+    {
+        protected override void Seed(ModelContext context)
+        {
+            base.Seed(context);
+            SeedUsers(context);
+            SaveCommonMeta(context);
+            context.SaveChanges();
+        }
+
+        private static void SeedUsers(ModelContext context)
+        {
+            context.Users.Add(new User {Username = "Admin", Password = "raw", Name = "The Fucking Admin"});
+        }
+
+        private static void SaveCommonMeta(ModelContext context)
+        {
+            var authors = new List<Author>
+            {
+                new Author {Name = "Toan Nguyen"},
+                new Author {Name = "Ke Huy Diet"},
+                new Author {Name = "Super Author"},
+            };
+            context.Authors.AddRange(authors);
+
+
+            var publishers = new List<Publisher>
+            {
+                new Publisher {Name = "Publisher Toan Nguyen"},
+                new Publisher {Name = "Kim Dong"},
+                new Publisher {Name = "Super Pulisher"},
+            };
+            context.Publishers.AddRange(publishers);
+
+            var categories = new List<Category>
+            {
+                new Category {Name = "Fantasy"},
+                new Category {Name = "Scifi"},
+                new Category {Name = "Romance"},
+                new Category {Name = "Horror"},
+                new Category {Name = "Commic"},
+                new Category {Name = "History"},
+            };
+            context.Categories.AddRange(categories);
+
+            var series = new List<Series>
+            {
+                new Series {Name = "Doraemon"},
+                new Series {Name = "Harry Potter"},
+                new Series {Name = "Canh sat hinh su"},
+            };
+            context.Series.AddRange(series);
+
+            var storages = new List<Storage>
+            {
+                new Storage {Name = "On shelf"},
+                new Storage {Name = "In Storage"},
+            };
+            context.Storages.AddRange(storages);
+        }
+    }
+}
---
+++ WinformsLibraryManager/Model/Meta/Series.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WinFormsLibraryManager.Model.Meta
+{
+    public class Series : IAuditable, INamed
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        [Required, Index(IsUnique = true), Column(TypeName = "VARCHAR"), MaxLength(256)]
+        public string Name { get; set; }
+    }
+}
