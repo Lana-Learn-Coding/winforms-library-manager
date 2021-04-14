6e7004fed4f5032a4e2b478e90216825c4c3ec4b
lana <sitdownrightnow.dev@gmail.com>
2021-04-15 02:01:38 +0700

    feat: add storage and position combobox control

---
+++ LibraryApplication/UI/Component/Combobox/PositionComboboxControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+using LibraryApplication.Model.Meta;
+
+namespace LibraryApplication.UI.Component.Combobox
+{
+    public class PositionComboboxControl : BaseComboboxControl<Position>
+    {
+        protected override ObservableCollection<Position> LocalContext => Context.Positions.Local;
+
+        public PositionComboboxControl()
+        {
+            Hint = "Select Position";
+            Label = "Position";
+        }
+    }
+}
---
+++ LibraryApplication/UI/Component/Combobox/StorageComboboxControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+using LibraryApplication.Model.Meta;
+
+namespace LibraryApplication.UI.Component.Combobox
+{
+    public class StorageComboboxControl : BaseComboboxControl<Storage>
+    {
+        protected override ObservableCollection<Storage> LocalContext => Context.Storages.Local;
+
+        public StorageComboboxControl()
+        {
+            Hint = "Select Storage";
+            Label = "Storage";
+        }
+    }
+}
