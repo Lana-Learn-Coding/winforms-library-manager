e4bf05c16cb65351f4cb22215a96f1814e232d07
lana <sitdownrightnow.dev@gmail.com>
2021-04-15 22:54:25 +0700

    feat: implement reader crud logic and binding

--- LibraryApplication/UI/View/Reader/ReaderManageView.cs
+++ LibraryApplication/UI/View/Reader/ReaderManageView.cs
@@ -1,24 +1,81 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
+using System.Reactive.Disposables;
 using System.Windows.Forms;
+using MaterialSkin.Controls;
 using ReactiveUI;
+using ReactiveUI.Validation.Extensions;
 
 namespace LibraryApplication.UI.View.Reader
 {
-    public partial class ReaderManageView : UserControl, IViewFor<ReaderManageViewModel>
+    public partial class ReaderManageView : UserControl, IDataFormView<Model.Book.Reader, ReaderManageViewModel>
     {
         public ReaderManageView()
         {
             InitializeComponent();
+            selectGender.InitializeDataSource();
+            ((IDataFormView<Model.Book.Reader, ReaderManageViewModel>) this).InitializeViewModelBindings();
+            this.WhenActivated(disposable =>
+            {
+                this.OneWayBind(ViewModel, model => model.SelectedItem.Id, view => view.txtID.Value)
+                    .DisposeWith(disposable);
+
+                this.BindCommand(ViewModel, model => model.ToggleBorrowBookCommand, view => view.btnBorrow)
+                    .DisposeWith(disposable);
+
+                this.Bind(ViewModel, model => model.SelectedItem.Avatar, view => view.imgPicker.Value)
+                    .DisposeWith(disposable);
+
+                this.Bind(ViewModel, model => model.SelectedItem.Name, view => view.txtName.Value)
+                    .DisposeWith(disposable);
+                this.BindValidation(ViewModel, model => model.SelectedItem.Name, view => view.txtName.Error)
+                    .DisposeWith(disposable);
+
+                this.Bind(ViewModel, model => model.SelectedItem.Birth, view => view.dateBirthdate.Value)
+                    .DisposeWith(disposable);
+                this.BindValidation(ViewModel, model => model.SelectedItem.Birth, view => view.dateBirthdate.Error)
+                    .DisposeWith(disposable);
+
+                this.Bind(ViewModel, model => model.SelectedItem.Gender, view => view.selectGender.Value)
+                    .DisposeWith(disposable);
+                this.BindValidation(ViewModel, model => model.SelectedItem.Gender, view => view.selectGender.Error)
+                    .DisposeWith(disposable);
+
+                this.Bind(ViewModel, model => model.SelectedItem.Email, view => view.txtEmail.Value)
+                    .DisposeWith(disposable);
+                this.BindValidation(ViewModel, model => model.SelectedItem.Email, view => view.txtEmail.Error)
+                    .DisposeWith(disposable);
+
+                this.Bind(ViewModel, model => model.SelectedItem.PhoneNumber, view => view.txtPhone.Value)
+                    .DisposeWith(disposable);
+                this.BindValidation(ViewModel, model => model.SelectedItem.PhoneNumber, view => view.txtPhone.Error)
+                    .DisposeWith(disposable);
+
+                this.Bind(ViewModel, model => model.SelectedItem.Limit, view => view.txtLimit.Value)
+                    .DisposeWith(disposable);
+                this.BindValidation(ViewModel, model => model.SelectedItem.Limit, view => view.txtLimit.Error)
+                    .DisposeWith(disposable);
+
+                this.WhenAnyValue(v => v.ViewModel.ShowBorrowBookDialog)
+                    .Subscribe(show =>
+                    {
+                        //TODO: implement actual borrow dialog
+                        if (!show) return;
+                        var form = new Form();
+                        form.ShowDialog(this);
+                        ViewModel.ShowBorrowBookDialog = false;
+                    })
+                    .DisposeWith(disposable);
+            });
         }
 
         [Browsable(false)]
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
-        public ReaderManageViewModel ViewModel { get; set; }
+        public ReaderManageViewModel ViewModel { get; set; } = new();
 
-        object IViewFor.ViewModel
-        {
-            get => ViewModel;
-            set => ViewModel = (ReaderManageViewModel) value;
-        }
+        public MaterialButton BtnSave => btnSave;
+        public MaterialButton BtnDelete => btnDelete;
+        public MaterialButton BtnClear => btnClear;
+        public DataGridView Table => table;
     }
 }
--- LibraryApplication/UI/View/Reader/ReaderManageViewModel.cs
+++ LibraryApplication/UI/View/Reader/ReaderManageViewModel.cs
@@ -1,8 +1,105 @@
-﻿using ReactiveUI;
+﻿using System;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Text.RegularExpressions;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using ReactiveUI.Validation.Extensions;
+using ReactiveUI.Validation.States;
 
 namespace LibraryApplication.UI.View.Reader
 {
-    public class ReaderManageViewModel : ReactiveObject
+    public class ReaderManageViewModel : DataFormViewModel<Model.Book.Reader>
     {
+        [Reactive] public bool ShowBorrowBookDialog { get; set; }
+
+        public readonly ReactiveCommand<Unit, bool> ToggleBorrowBookCommand;
+
+        public ReaderManageViewModel()
+        {
+            Items = Context.Readers.Local;
+            this.ValidationRule(
+                model => model.SelectedItem.Name,
+                title => !string.IsNullOrWhiteSpace(title),
+                "Name is required"
+            );
+            this.ValidationRule(
+                model => model.SelectedItem.Limit,
+                year => year > 0,
+                "Year must > 0"
+            );
+            this.ValidationRule(
+                model => model.SelectedItem.Gender,
+                val => val != null,
+                "Please select Gender"
+            );
+            this.ValidationRule(
+                model => model.SelectedItem.Birth,
+                val => val != null,
+                "Please provide birth date"
+            );
+            this.ValidationRule(
+                model => model.SelectedItem.Email,
+                this.WhenAnyValue(model => model.SelectedItem.Email)
+                    .Select(ValidateEmail)
+            );
+            this.ValidationRule(
+                model => model.SelectedItem.PhoneNumber,
+                this.WhenAnyValue(model => model.SelectedItem.PhoneNumber)
+                    .Select(ValidatePhone)
+            );
+
+            ToggleBorrowBookCommand = ReactiveCommand.Create(() => ShowBorrowBookDialog = !ShowBorrowBookDialog);
+        }
+
+        private static ValidationState ValidateEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                return new ValidationState(false, "Email is required");
+            }
+
+            var emailRegex = new Regex(".+@.+");
+            return !emailRegex.IsMatch(email)
+                ? new ValidationState(false, "Invalid email format")
+                : ValidationState.Valid;
+        }
+
+        private static ValidationState ValidatePhone(string phone)
+        {
+            if (string.IsNullOrWhiteSpace(phone))
+            {
+                return new ValidationState(false, "Phone is required");
+            }
+
+            var phoneRegex = new Regex("0[0-9]{9,10}");
+            return !phoneRegex.IsMatch(phone)
+                ? new ValidationState(false, "Invalid phone number format")
+                : ValidationState.Valid;
+        }
+
+        protected override void OnSaving()
+        {
+            if (Context.Readers.Any(reader =>
+                reader.Id != SelectedItem.Id && reader.PhoneNumber == SelectedItem.PhoneNumber))
+            {
+                throw new Exception("Phone number was already taken");
+            }
+
+            if (Context.Readers.Any(reader =>
+                reader.Id != SelectedItem.Id && reader.Email == SelectedItem.Email))
+            {
+                throw new Exception("Email was already taken");
+            }
+        }
+
+        protected override void OnDeleting()
+        {
+            if (SelectedItem.Tickets.Any(ticket => ticket.ReturnedDate is null))
+            {
+                throw new Exception("User is borrowing some book");
+            }
+        }
     }
 }
