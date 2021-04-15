655c0b1599066c97d13b510aa30c29eb8aaa3c88
lana <sitdownrightnow.dev@gmail.com>
2021-04-15 20:56:02 +0700

    feat: add gender combobox

---
+++ LibraryApplication/UI/Component/Combobox/GenderCombobox.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+using LibraryApplication.Model.Meta;
+
+namespace LibraryApplication.UI.Component.Combobox
+{
+    public class GenderCombobox : BaseComboboxControl<Gender>
+    {
+        protected sealed override ObservableCollection<Gender> LocalContext => Context.Genders.Local;
+
+        public GenderCombobox()
+        {
+            Hint = "Select Gender";
+            Label = "Gender";
+        }
+    }
+}
