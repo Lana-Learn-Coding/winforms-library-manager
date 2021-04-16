0f805f8af482571e153b0e0a8aaafe27abf33f8f
lana <sitdownrightnow.dev@gmail.com>
2021-04-16 21:25:57 +0700

    feat: add custom datagridview formatted column wrapper for textbox column

---
+++ LibraryApplication/UI/Component/Table/DataGridViewFormattedColumn.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace LibraryApplication.UI.Component.Table
+{
+    public class DataGridViewFormattedColumn : DataGridViewTextBoxColumn
+    {
+        [DefaultValue("")]
+        [Category("Behavior")]
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        public string Format
+        {
+            get => DefaultCellStyle.Format;
+            set => DefaultCellStyle.Format = value;
+        }
+    }
+}
