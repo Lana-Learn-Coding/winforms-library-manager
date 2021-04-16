6dcd84cc0a669c3a1224d50f85dcd28734d0bf5a
lana <sitdownrightnow.dev@gmail.com>
2021-04-17 01:19:16 +0700

    feat: add datagridview date time column for display datetime

---
+++ LibraryApplication/UI/Component/Table/DataGridViewDateTimeColumn.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace LibraryApplication.UI.Component.Table
+{
+    public class DataGridViewDateTimeColumn : DataGridViewFormattedColumn
+    {
+        [DefaultValue("yyyy-MM-dd HH:mm:ss")]
+        [Category("Behavior")]
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        public new string Format
+        {
+            get => base.Format;
+            set => base.Format = value;
+        }
+
+        public DataGridViewDateTimeColumn()
+        {
+            Format = "yyyy-MM-dd HH:mm:ss";
+            ValueType = typeof(DateTime?);
+        }
+    }
+}
