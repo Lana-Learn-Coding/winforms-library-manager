17936fb27681cfa3c502042a522c15f7877984e8
lana <sitdownrightnow.dev@gmail.com>
2021-04-18 03:28:10 +0700

    refactor: rename model context to context

--- LibraryApplication/UI/View/Reader/BorrowingManageDialogModel.cs
+++ LibraryApplication/UI/View/Reader/BorrowingManageDialogModel.cs
@@ -7,13 +7,13 @@ namespace LibraryApplication.UI.View.Reader
     public partial class BorrowingManageDialogModel : ReactiveObject, IActivatableViewModel
     {
         public ViewModelActivator Activator { get; } = new();
-        private ModelContext ModelContext { get; }
-        private Model.Book.Reader Reader { get; }
+        protected ModelContext Context { get; }
+        protected Model.Book.Reader Reader { get; }
 
         public BorrowingManageDialogModel(Model.Book.Reader reader)
         {
             Reader = reader;
-            ModelContext = Locator.Current.GetService<ModelContext>();
+            Context = Locator.Current.GetService<ModelContext>();
             InitializeNewTicketTab();
         }
     }
