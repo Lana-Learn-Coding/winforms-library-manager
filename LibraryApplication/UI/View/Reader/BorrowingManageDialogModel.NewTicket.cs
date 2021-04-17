23c1344aec843a30b9227211776115e8791d3ae0
lana <sitdownrightnow.dev@gmail.com>
2021-04-18 03:29:02 +0700

    feat: rename model context to context, add AvailableBook Management logic

--- LibraryApplication/UI/View/Reader/BorrowingManageDialogModel.NewTicket.cs
+++ LibraryApplication/UI/View/Reader/BorrowingManageDialogModel.NewTicket.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Reactive;
 using System.Windows;
+using DynamicData;
 using LibraryApplication.Model.Book;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
@@ -9,15 +12,28 @@ namespace LibraryApplication.UI.View.Reader
 {
     public partial class BorrowingManageDialogModel
     {
+        [Reactive] public ObservableCollection<BookItem> BorrowableBooks { get; set; } = new();
         [Reactive] public Ticket NewTicket { get; set; } = new();
 
-        public ReactiveCommand<Unit, Unit> SaveNewTicketCommand;
-        public ReactiveCommand<Unit, Unit> ClearNewTicketCommand;
+        public ReactiveCommand<int, Unit> AddBookToListCommand { get; set; }
+        public ReactiveCommand<Unit, Unit> SaveNewTicketCommand { get; set; }
+        public ReactiveCommand<Unit, Unit> ClearNewTicketCommand { get; set; }
 
         private void InitializeNewTicketTab()
         {
+            BorrowableBooks =
+                new ObservableCollection<BookItem>(Context.BookItems.Local.Where(item => item.BorrowingTicket == null));
             SaveNewTicketCommand = ReactiveCommand.Create(SaveNewTicket);
-            ClearNewTicketCommand = ReactiveCommand.Create(() => { NewTicket = new Ticket(); });
+            ClearNewTicketCommand = ReactiveCommand.Create(ClearNewTicket);
+            AddBookToListCommand = ReactiveCommand.Create<int>(AddSelectedBookToList);
+        }
+
+        private void AddSelectedBookToList(int id)
+        {
+            if (id <= 0) return;
+            var bookItem = BorrowableBooks.First(book => book.Id == id);
+            NewTicket.BookItems.Add(bookItem);
+            BorrowableBooks.Remove(bookItem);
         }
 
         private void SaveNewTicket()
@@ -36,10 +52,16 @@ namespace LibraryApplication.UI.View.Reader
 
             NewTicket.BorrowedDate = DateTime.Now;
             NewTicket.Reader = Reader;
-            ModelContext.Tickets.Add(NewTicket);
-            ModelContext.SaveChanges();
+            Context.Tickets.Add(NewTicket);
+            Context.SaveChanges();
             MessageBox.Show($"Ticket {NewTicket.Id} created. Due date: {NewTicket.DueDate.Value:yyyy-MM-dd}",
                 "Success");
+            NewTicket = new Ticket();
+        }
+
+        private void ClearNewTicket()
+        {
+            BorrowableBooks.AddRange(NewTicket.BookItems);
             NewTicket = new Ticket();
         }
     }
